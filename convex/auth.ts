341ee8690bca145aea7dd98ca11272a505554c7e
vyeos <rudranpatel0980@gmail.com>
2026-01-13 23:32:17 +0530

    edit: default values for allowance, energy, isVirgin

--- convex/auth.ts
+++ convex/auth.ts
@@ -4,5 +4,46 @@ import Google from "@auth/core/providers/google";
 import { Password } from "@convex-dev/auth/providers/Password";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [GitHub, Google, Password],
+  providers: [
+    GitHub({
+      profile(profile) {
+        return {
+          id: profile.id.toString(),
+          name: profile.name,
+          email: profile.email,
+          image: profile.avatar_url,
+          // Custom fields
+          daily_allowance: 20,
+          energy: 20,
+          isVirgin: false,
+        };
+      },
+    }),
+    Google({
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+          // Custom fields
+          daily_allowance: 20,
+          energy: 20,
+          isVirgin: false,
+        };
+      },
+    }),
+    Password({
+      profile(params) {
+        return {
+          email: params.email as string,
+          name: params.name as string,
+          // Custom fields
+          daily_allowance: 20,
+          energy: 20,
+          isVirgin: false,
+        };
+      },
+    }),
+  ],
 });
