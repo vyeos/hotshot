51779254638d81394cba1b66fec8d3d1a9c2857f
vyeos <rudranpatel0980@gmail.com>
2026-01-13 22:05:06 +0530

    add: 0Auth - github, google

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,14 +1,16 @@
+import { convexAuth } from "@convex-dev/auth/server";
+import GitHub from "@auth/core/providers/github";
+import Google from "@auth/core/providers/google";
+
+export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
+  providers: [GitHub, Google],
+});
+
 // import { action } from "./_generated/server";
 // import { internal } from "./_generated/api";
 // import { ConvexError, v } from "convex/values";
 // import bcrypt from "bcryptjs";
 // import { Doc } from "./_generated/dataModel";
-
-import { convexAuth } from "@convex-dev/auth/server";
-
-export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [],
-});
 
 // export const signUp = action({
 //   args: {
